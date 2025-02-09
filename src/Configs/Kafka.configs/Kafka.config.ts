47d54c8697278f80e00c97e5f0bfe0783dae6110
Jackson Cheriyan Kollareth <138844080+ChackzWolf@users.noreply.github.com>
2025-02-10 04:48:20 +0530

    solve: case problem controller, utils

--- src/Configs/Kafka.configs/Kafka.config.ts
+++ src/Configs/Kafka.configs/Kafka.config.ts
@@ -13,7 +13,6 @@ export class KafkaConfig {
       brokers: ['education-kafka.dowhilelearn.svc.cluster.local:29092'],
       retry: {
         maxRetryTime: 60000, // 60 seconds
-      
       },
       connectionTimeout: 10000, // 10 seconds
       requestTimeout: 25000, // 25 seconds
